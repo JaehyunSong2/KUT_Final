7195766d51504401d13d0977d524f834cdcbbd56
JaehyunSong2 <t210047@kansai-u.ac.jp>
2025-08-31 11:20:01 +0900

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# Repository for Binder with R 4.3.3
+
+[![Binder](http://mybinder.org/badge_logo.svg)](https://binder.cs.rcos.nii.ac.jp/v2/gh/JaehyunSong/KUT_Final/HEAD)
+
+- 高知工科大学夏季集中講義「計量経済学」用レポジトリー
---
+++ setup.R
@@ -0,0 +1,3 @@
+tinytex::tlmgr_install("haranoaji")
+install.packages("coefplot", dependencies = TRUE, repos = "https://cran.rstudio.com/")
+install.packages("interplot", dependencies = TRUE, repos = "https://cran.rstudio.com/")
